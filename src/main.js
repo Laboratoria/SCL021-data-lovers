65b5a0879e294f899f9fa616fb3664781138b17b
Your Name <you@example.com>
2022-07-30 17:54:54 -0400

    Co-authored-by: Andrea Santos <dreasantos51@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+ import data from './data/athletes/athletes.js';
+ const divRoot = document.getElementById("root");//div de html donde guardare objetos de js
+ //const fragment = document.createDocumentFragment()
+ //console.log(data.athletes["age"]);
 
-console.log(example, data);
+ const pais = data.athletes[i].team;
+ const nuevoParrafo = document.createElement("p");
+ nuevoParrafo.className.add(pais);
+
+ 
+ const atleta = data.athletes[i].name;
+ const deporte = data.athletes[i].sport;
+ const medalla = data.athletes[i].medal;
+
+
+
+//  pais.array.forEach(paises => {
+//     const parrafo = document.createElement('p');
+//     parrafo.className=pais;
+    
+//  });
+
+
+ for(let i = 0; i <= data.athletes.length; i++){
+     console.log(data.athletes[i].team);
+     console.log(data.athletes[i].name);
+     console.log(data.athletes[i].sport);
+     console.log(data.athletes[i].medal);
+    
+
+
+ }
