3e4b27c65343bf54273820aa0b3a2d7b6035693d
Jazmin Robles <jazmasoterapeuta@gamail.com>
2022-08-01 18:19:40 -0400

    cambios/insertamos la data en el html/se cambio la estructura de html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 
  import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data.results)
+const dataRickAndMorty = data.results;
+
+const selectBody = document.getElementById("personajes")
+const selectsBody = document.getElementById("especies")
+for (var i = 0; i < dataRickAndMorty.length; i++) {
+selectBody.innerHTML += `
+
+<select>
+      <option>${dataRickAndMorty[i].name}</option>
+    </select>
+    
+    
+`;
+
+selectsBody.innerHTML += `
+
+<select>
+      <option>${dataRickAndMorty[i].species}</option>
+    </select>
+
+`;
+}
+
+console.log(dataRickAndMorty.length)
+ 
