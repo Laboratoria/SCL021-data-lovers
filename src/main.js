0516d741023819c6c46399e78f970b86ee81a622
Andrea Santos <dreasantos51@gmail.com>
2022-08-01 15:55:58 -0400

    test

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@
 
 import data from './data/athletes/athletes.js';
 
-for (let i = 0; i<= data.athletes.length;i++){
-  console.log(data.athletes[i].team);
-  console.log(data.athletes[i].name);
-  console.log(data.athletes[i].sport);
-  console.log(data.athletes[i].medal);
+//for (let i = 0; i<= data.athletes.length;i++){
+ // console.log(data.athletes[i].team);
+ // console.log(data.athletes[i].name);
+ // console.log(data.athletes[i].sport);
+ // console.log(data.athletes[i].medal);
 
-}
-console.log(data.athletes);
+//}
+//console.log(data.athletes);
