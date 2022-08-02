85aeb39fb1e0fc48e1fc7a3ed2bf504b9905389e
Your Name <you@example.com>
2022-08-02 08:49:30 -0400

    ultimos cambios

--- src/main.js
+++ src/main.js
@@ -1,18 +1,65 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
- import data from './data/athletes/athletes.js';
- const divRoot = document.getElementById("root");//div de html donde guardare objetos de js
- //const fragment = document.createDocumentFragment()
- //console.log(data.athletes["age"]);
+  import data from './data/athletes/athletes.js';
+  const divRoot = document.getElementById("root");//div de html donde guardare objetos de js
+  const fragment = document.createDocumentFragment()
+  const liTemplate = document.getElementById("liTemplate")//capturamos template de html
 
- const pais = data.athletes[i].team;
- const nuevoParrafo = document.createElement("p");
- nuevoParrafo.className.add(pais);
+  const teamAthletes = data.athletes;
+//   const result = data.athletes.reduce(function (allTeam, athletes) {//aqui se crea la const para aplicar el reduce y queremos traer el pais 
+//     return Array.from(new Set([...allTeam, ...athletes.team]))//deberia retornar solo los paises sin repetirse 
+//   }, [])
+
+
+
+function botonPais(){
+    paises
+}
+
+document.getElementById("btnPais").onclick = function(){
+    botonPais();
+}
+
+
+  const prueba = teamAthletes.map(function(item){
+    return item.team
+  })
+  const paises = new Set ([...prueba])
+
+
+  console.log(paises)
+  
+
+   
 
  
- const atleta = data.athletes[i].name;
- const deporte = data.athletes[i].sport;
- const medalla = data.athletes[i].medal;
+ 
+
+  //console.log(newTeamAthletes)
+  
+
+  teamAthletes.forEach(paises => {
+     const clone = liTemplate.content.cloneNode(true)
+     clone.getElementById("text-primary").textContent = paises.team
+
+     fragment.appendChild(clone)
+ })
+
+ divRoot.appendChild(fragment)
+ 
+    
+    
+    
+ 
+ //console.log(data.athletes["age"]);
+
+//  const nuevoParrafo = document.createElement("p");
+//  nuevoParrafo.className.add(pais);
+
+ 
+//  const atleta = data.athletes[i].name;
+//  const deporte = data.athletes[i].sport;
+//  const medalla = data.athletes[i].medal;
 
 
 
@@ -23,12 +70,10 @@
 //  });
 
 
- for(let i = 0; i <= data.athletes.length; i++){
-     console.log(data.athletes[i].team);
-     console.log(data.athletes[i].name);
-     console.log(data.athletes[i].sport);
-     console.log(data.athletes[i].medal);
+//   for(let i = 0; i <= data.athletes.length; i++){
+//       console.log(data.athletes[i].team);
+//      console.log(data.athletes[i].name);
+//      console.log(data.athletes[i].sport);
+//      console.log(data.athletes[i].medal);
     
-
-
- }
+  //}
