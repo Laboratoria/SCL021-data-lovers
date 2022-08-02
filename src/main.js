60de0dd63eca3dd2034c28e37ec516e166b0e28b
LissetteQ <lissetteq123@gmail.com>
2022-08-02 10:24:34 -0400

    agregar mainJS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
-import { example } from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';
-console.log(example, data);
+//import { example } from './data.js';
+
+import data from "./data/rickandmorty/rickandmorty.js";
+
+//console.log(data.results);
+const dataRickAndMorty = data.results;
+const especies = new Set()
+const selectBody = document.getElementById("personajes");
+const selectsBody = document.getElementById("especies");
+for (var i = 0; i < dataRickAndMorty.length; i++) {
+    especies.add(dataRickAndMorty[i].species);
+
+    selectBody.innerHTML += `
+
+<select>
+      <option>${dataRickAndMorty[i].name}</option>
+    </select>
+    
+    
+`;
+}
+
+for (let i = 0; i < especies.size; i++) {
+    selectsBody.innerHTML += `
+
+<select>
+      <option>${especies}</option>
+    </select>
+
+`;
+}
+
+//console.log(dataRickAndMorty.length);
