a1f4990ac8c9449ce08046ab7af1025c23d6bbe0
Jazmin Robles <jazmasoterapeuta@gamail.com>
2022-08-08 13:35:42 -0400

    eliminamos select de personajese en html/filtramos las especies en el select y poder seleccionar opciones,al seleccional se ven las card con datos filtrados/agregams cambios en css

--- src/main.js
+++ src/main.js
@@ -16,21 +16,30 @@ function getCards(arrItems) {
 
   for (let i = 0; i < arrItems.length; i++) {
     const card = document.createElement("div");
+    card.setAttribute("id","boxcard")
     const pName = document.createElement("p");
     pName.textContent = arrItems[i].name;
+    pName.setAttribute = ("class", "name")
     const pSpecies = document.createElement("p");
     pSpecies.textContent = arrItems[i].species;
+    const pStatus = document.createElement("p");
+    pStatus.textContent = arrItems[i].status;
+    const pGender = document.createElement("p");
+    pGender.textContent = arrItems[i].gender;
+    const pType = document.createElement("p");
+    pType.textContent = arrItems[i].type;
+   
 
-    
-    
     const imagen = document.createElement("img");
+
     imagen.src = arrItems[i].image;
 
     card.appendChild(imagen);
     card.appendChild(pName);
     card.appendChild(pSpecies);
-    
-
+    card.appendChild(pStatus);
+    card.appendChild(pGender);
+    card.appendChild(pType);
     arrCards.push(card);
   }
 
@@ -41,41 +50,37 @@ function getCards(arrItems) {
 for (var i = 0; i < dataRickAndMorty.length; i++) {
   especies.add(dataRickAndMorty[i].species);
 
-  selectorPersonajes.innerHTML += `
-<select>
-      <option>${dataRickAndMorty[i].name}</option>
-    </select>  
-`;
+
 }
 //este for pinta el selector de especies
 for (let especie of especies) {
   selectorEspecies.innerHTML += `
-<select>
+
       <option>${especie}</option>
-    </select>
+   
 `;
 }
-//este for solo pinta los primeros 5 personajes para las cajas
-const arrCards = getCards(dataRickAndMorty);
-for (let e = 0; e < 20; e++) {
-
+// idealmente que esto ocurra cuando hay evento onload
+function print(data){
+for (let e = 0; e < 5; e++) {
   //selectorRoot.innerHTML += `<div>${dataRickAndMorty[e].name}</div>`;
-  selectorRoot.appendChild(arrCards[e]);
+  selectorRoot.appendChild(getCards(data)[e]);
+}
 }
 
-selectorEspecies.addEventListener("change",function(){
-  console.log(selectorEspecies.value)
-  const dataFiltered = dataRickAndMorty.filter(function(character){
-    return character.species == selectorEspecies.value
-    
+print(dataRickAndMorty);
+
+selectorEspecies.addEventListener("change", function () {
+  document.getElementById("root").innerHTML = ""
+  //hacer que se impriman las nuevas
+  //console.log(selectorEspecies.value);
+  let dataFiltered = dataRickAndMorty.filter(function (character) {
+    return character.species === selectorEspecies.value;
   })
-  console.log (dataFiltered)
-  
-   
-
-})
-
-
+  //console.log
+  print(dataFiltered);
+ 
+});
 ///////////////////////////////////////////////////////////////
 
 
