9ca6fa78bb34220e192554e655cc3f7c66e89e8e
LissetteQ <lissetteq123@gmail.com>
2022-08-08 13:28:56 -0400

    Filtro de especies

--- src/data.js
+++ src/data.js
@@ -1,1 +1,4 @@
 //escribir funciones de filtro (filtro por personajes y filtro por especie)
+let dataFiltered = dataRickAndMorty.filter(function (character) {
+    return character.species === selectorEspecies.value;
+  })
