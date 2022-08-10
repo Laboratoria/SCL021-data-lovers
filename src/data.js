84bdc866acccf6070ed295ac7672a3c166bebefa
LissetteQ <lissetteq123@gmail.com>
2022-08-09 20:05:57 -0400

    filtro de datos

--- src/data.js
+++ src/data.js
@@ -1,4 +1,22 @@
-//escribir funciones de filtro (filtro por personajes y filtro por especie)
-let dataFiltered = dataRickAndMorty.filter(function (character) {
-    return character.species === selectorEspecies.value;
-  })
+//hacer que se impriman las nuevas
+//console.log(selectorEspecies.value);
+const datafilter = (characters, species) => {
+  let dataFiltered = characters.filter(function (character) {
+    return character.species === species;
+  });
+  return dataFiltered;
+};
+
+const sortData = (arrData, sortBy, sortOrder) => {
+  let arrResult = arrData.sort((a, b) => {
+    if (a[sortBy] > b.name) {
+      return 1;
+    } else if (a.name < b.name) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return arrResult;
+};
+export { datafilter };
--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-//import { example } from './data.js';
-
-import rickandmorty from "./data/rickandmorty/rickandmorty.js";
+import { datafilter } from "./data.js";
 import data from "./data/rickandmorty/rickandmorty.js";
 
 //console.log(data.results);
 const dataRickAndMorty = data.results;
 const especies = new Set();
 //selectores de los filtros y de la seccion donde se van a pintar los personajes
-const selectorPersonajes = document.getElementById("personajes");
 const selectorEspecies = document.getElementById("especiess");
 const selectorRoot = document.getElementById("root");
 
@@ -16,10 +13,10 @@ function getCards(arrItems) {
 
   for (let i = 0; i < arrItems.length; i++) {
     const card = document.createElement("div");
-    card.setAttribute("id","boxcard")
+    card.setAttribute("id", "boxcard");
     const pName = document.createElement("p");
     pName.textContent = arrItems[i].name;
-    pName.setAttribute = ("class", "name")
+    pName.setAttribute = ("class", "name");
     const pSpecies = document.createElement("p");
     pSpecies.textContent = arrItems[i].species;
     const pStatus = document.createElement("p");
@@ -28,7 +25,6 @@ function getCards(arrItems) {
     pGender.textContent = arrItems[i].gender;
     const pType = document.createElement("p");
     pType.textContent = arrItems[i].type;
-   
 
     const imagen = document.createElement("img");
 
@@ -49,8 +45,6 @@ function getCards(arrItems) {
 //este for pinta el selector de personajes
 for (var i = 0; i < dataRickAndMorty.length; i++) {
   especies.add(dataRickAndMorty[i].species);
-
-
 }
 //este for pinta el selector de especies
 for (let especie of especies) {
@@ -61,25 +55,21 @@ for (let especie of especies) {
 `;
 }
 // idealmente que esto ocurra cuando hay evento onload
-function print(data){
-for (let e = 0; e < 5; e++) {
-  //selectorRoot.innerHTML += `<div>${dataRickAndMorty[e].name}</div>`;
-  selectorRoot.appendChild(getCards(data)[e]);
-}
+function print(data) {
+  for (let e = 0; e < 20; e++) {
+    //selectorRoot.innerHTML += `<div>${dataRickAndMorty[e].name}</div>`;
+    selectorRoot.appendChild(getCards(data)[e]);
+  }
 }
 
 print(dataRickAndMorty);
 
 selectorEspecies.addEventListener("change", function () {
-  document.getElementById("root").innerHTML = ""
+  document.getElementById("root").innerHTML = "";
   //hacer que se impriman las nuevas
   //console.log(selectorEspecies.value);
-  let dataFiltered = dataRickAndMorty.filter(function (character) {
-    return character.species === selectorEspecies.value;
-  })
   //console.log
-  print(dataFiltered);
- 
+  print(datafilter(dataRickAndMorty, selectorEspecies.value));
 });
 
 //crear los listener de los filtros selectores (personajes y especie)
