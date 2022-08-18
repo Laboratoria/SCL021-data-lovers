51a457a35da2401cef41f4f78d99e080981dd62a
Your Name <you@example.com>
2022-08-18 13:12:43 -0400

    boton ordenar

--- src/data.js
+++ src/data.js
@@ -2,33 +2,33 @@
 
 export const filterData = (data, country, sport, medal) => {
 
-  return data.filter((element)=>{
-    if (sport==="Deporte") sport = false;
-    if (country==="Pais") country = false;
-    if (medal==="Medalla") medal = false;
+  return data.filter((element) => {
+    if (sport === "Deporte") sport = false;
+    if (country === "País") country = false;
+    if (medal === "Medalla") medal = false;
 
-    if(country && sport && medal)
+    if (country && sport && medal)
       return element.team === country && element.sport === sport && element.medal === medal
 
-    if(sport && medal)
+    if (sport && medal)
       return element.sport === sport && element.medal === medal
 
-    if(country && sport)
-       return element.team === country && element.sport === sport
+    if (country && sport)
+      return element.team === country && element.sport === sport
 
-    if(country && medal)
-       return element.team === country && element.medal === medal
+    if (country && medal)
+      return element.team === country && element.medal === medal
 
     if (country)
-       return element.team === country
+      return element.team === country
 
     if (sport)
-       return element.sport === sport
+      return element.sport === sport
 
     if (medal)
-       return element.medal === medal
+      return element.medal === medal
 
   })
- };
+};
 
 
--- src/main.js
+++ src/main.js
@@ -1,39 +1,88 @@
 //import { example } from './data.js';
 import data from './data/athletes/athletes.js';
 import { filterData } from './data.js';
+//import { sortDataUp } from './data.js';
+//  import {sortDataDown } from './data.js';
 
 const teamAthletes = data.athletes;//trae la base de datos de atlethes
 
-function sortData(data, columna="team"){
+function sortData(data, columna = "team") {
     data.sort((a, b) => {
         return a[columna] < b[columna] ? -1 : 1;
     })
 }
-sortData(teamAthletes, );
+sortData(teamAthletes);
 
-function showAthletes(lista){
-    let medalsTable=document.getElementById("medalsTable");
-    let htmlmedals="";
-    lista.forEach(function(pais){
-      htmlmedals = htmlmedals + `
+//ORDENA DE A-Z
+function sortName(data, columna = "name") {
+    return data.sort((a, b) => {
+        return a[columna] < b[columna] ? -1 : 1;
+    })
+}
+//ORDENA DE Z-A
+function sortNameReverse(data, columna = "name") {
+    const sortedData = sortName(data)
+    sortedData.reverse()
+    return sortedData
+
+}
+
+// function sortOrder(data, columna = "name") {
+//     return sortData
+// }
+
+//funcion de select ordenar
+function handleChangeOrder() {
+    //tomar valor de select que escoja el usuario
+    //console.log("hola");
+
+    const selectName = document.getElementById('order').value;
+    //     //con ese valor comparar si tengo que ordenar ascendente o descendente
+    if (selectName === "order-up") {
+        const ordenAZ = sortName(teamAthletes)
+        showAthletes(ordenAZ)
+    }
+    if (selectName === "order-down") {
+        const ordenZA = sortNameReverse(teamAthletes)
+        showAthletes(ordenZA)
+    }
+    if (selectName === "totalOrder") {
+        const ordenNuevo = sortOrder(teamAthletes)
+        showAthletes(ordenNuevo)
+    }
+
+}
+
+const selectName = document.getElementById('order');
+selectName.addEventListener('change', handleChangeOrder);
+//     //console.log(handleChangeOrder);
+
+//funcion que muestra la DB en una tabla
+function showAthletes(lista) {
+    let medalsTable = document.getElementById("medalsTable");
+    let htmlmedals = "";
+    lista.forEach(function (pais) {
+        htmlmedals = htmlmedals + `
       <tr>
-      <td class="pais">${pais.team}</td>
-      <td class="nombre">${pais.name}</td>
-      <td class="deporte">${pais.sport}</td>
-      <td class="medalla">${pais.medal}</td>
+      
+      <td class="paisLista">${pais.team}</td>
+      <td class="nombreLista">${pais.name}</td>
+      <td class="deporteLista">${pais.sport}</td>
+      <td class="medallaLista">${pais.medal}</td>
       </tr>
       `
 
     })
-    medalsTable.innerHTML=htmlmedals
-  }
-  showAthletes(teamAthletes);
+    medalsTable.innerHTML = htmlmedals
+}
+showAthletes(teamAthletes);
 
 
 let country = "";
 let sport = "";
 let medal = "";
 
+//funcion para cambiar los filtros
 function handleChange(event) {
     //console.log(event.target.value)
     if (event.target.name === "country") {
@@ -47,29 +96,26 @@ function handleChange(event) {
     }
 
     let filterTeam = filterData(teamAthletes, country, sport, medal);
-    if (country === "Pais"){
+    if (country === "País") {
 
         showAthletes(teamAthletes)
-    }else{
-        if (sport === "Deporte"){
-
-            showAthletes(filterTeam);
-         } //else{
-
-        //     showAthletes(filterTeam);
-        // }
-    }
-        if (medal === "Medalla"){
-
-            showAthletes(filterTeam);
-        } else{
+    } else {
+        if (sport === "Deporte") {
 
             showAthletes(filterTeam);
         }
-        if ( country === "Pais" && sport === "Deporte" && medal === "Medalla"){
-           showAthletes(teamAthletes);
-        }
     }
+    if (medal === "Medalla") {
+
+        showAthletes(filterTeam);
+    } else {
+
+        showAthletes(filterTeam);
+    }
+    if (country === "País" && sport === "Deporte" && medal === "Medalla") {
+        showAthletes(teamAthletes);
+    }
+}
 
 
 
@@ -80,6 +126,9 @@ const selectSport = document.getElementById('deporte');
 selectSport.addEventListener('change', handleChange);
 const selectMedal = document.getElementById('medalla');
 selectMedal.addEventListener('change', handleChange);
+
+
+
 
 //funcion que hace un array con todos los paises de la DB sin repetirlos
 const prueba = teamAthletes.map(function (item) {
@@ -131,6 +180,7 @@ function cargarMedallas() {
     }
 }
 cargarMedallas();
+
 
 
 
